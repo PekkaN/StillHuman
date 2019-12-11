a4180ae7237dc971b7c4c2da73d03c14cf8b2453
PekkaN <pm.nordberg@gmail.com>
2019-12-11 16:02:16 +0200

    Initial dump

---
+++ Assets/Cam_Follow_player.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cam_Follow_player : MonoBehaviour
+{
+
+    [SerializeField]    
+    GameObject player;
+
+     [SerializeField]   
+    Vector2 posOffset;
+
+    [SerializeField]
+    float timeOfset;
+
+    public bool faceRight;
+
+    bool isCrouching;
+
+    private Vector3 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        faceRight = player.GetComponent<PlayerController_2d>().faceRight;
+        isCrouching = player.GetComponent<PlayerController_2d>().isCrouching;
+        //Cameras start position
+        Vector3 startPos = transform.position;
+
+    //player current position
+    
+        Vector3 endPos = player.transform.position;
+        endPos.x += posOffset.x;
+        endPos.y += posOffset.y;
+        endPos.z = -10;
+
+    if (faceRight){
+        endPos.x += posOffset.x*-2;
+
+}
+
+    if (isCrouching){
+        endPos.y += -2;
+    }
+        //this is how you lerp kuulemma
+        //transform.position = Vector3.Lerp(startPos,endPos,timeOfset * Time.deltaTime);
+
+        //this is how you use smooth dampening
+        transform.position = Vector3.SmoothDamp(startPos,endPos, ref velocity, timeOfset);
+
+
+        //transform.position = new Vector3(player.transform.position.x, player.transform.position.y,-10);
+
+
+
+    }
+}
---
+++ Assets/PlayerController_2d.cs
@@ -0,0 +1,156 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class PlayerController_2d : MonoBehaviour { 
+
+    Animator animator;
+    Rigidbody2D rb2d;
+    SpriteRenderer spriteRenderer;
+
+  
+    private float runMultiplier = 2;
+    private float defSpeed = 3;
+
+    private float moveSpeed = 3;
+
+    private float defJump = 5;
+
+     bool isGrounded;
+     bool canMove;
+     bool hanging;
+     bool canLedgeGrab;
+
+     public bool isCrouching;
+
+     public bool faceRight;
+
+// Ground checks set
+    [SerializeField]
+    Transform groundCheckC;
+      [SerializeField]
+    Transform groundCheckL;
+      [SerializeField]
+    Transform groundCheckR;
+
+          [SerializeField]
+    Transform grabCheckL;
+        [SerializeField]
+    Transform grabCheckR;
+
+              [SerializeField]
+    Transform grabCheckLnowall;
+        [SerializeField]
+    Transform grabCheckRnowall;
+
+
+            
+
+    // Start is called before the first frame update
+    void Start() {
+
+        animator = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+
+            
+    }
+
+
+    private void FixedUpdate() {
+
+// Grounded check
+        if((Physics2D.Linecast(transform.position, groundCheckC.position, 1<< LayerMask.NameToLayer("Ground"))) || 
+        (Physics2D.Linecast(transform.position, groundCheckL.position, 1<< LayerMask.NameToLayer("Ground"))) ||
+        (Physics2D.Linecast(transform.position, groundCheckR.position, 1<< LayerMask.NameToLayer("Ground"))))    
+        {
+            isGrounded = true;
+            canLedgeGrab = false;
+           // Debug.Log("maasa");
+        } else {
+            isGrounded = false;
+        }
+    
+
+// Movement
+        if(Input.GetKey("d") && canMove)
+        {
+            rb2d.velocity = new Vector2(moveSpeed, rb2d.velocity.y);
+             if (isGrounded)
+            animator.Play("Anim_Player_run");
+            spriteRenderer.flipX = false;
+            faceRight = false;
+       
+        }
+        else if (Input.GetKey("a")  && canMove)
+        {
+            rb2d.velocity = new Vector2(moveSpeed*-1, rb2d.velocity.y);
+             if (isGrounded)
+            animator.Play("Anim_Player_run");
+            spriteRenderer.flipX = true;
+            faceRight = true;
+        }
+
+        else{
+            if ((isGrounded) && !isCrouching)
+            animator.Play("Anim_Player_idle");
+            rb2d.velocity = new Vector2(0, rb2d.velocity.y);
+        }
+
+        if(Input.GetKey("space") && isGrounded)
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, 5);
+            animator.Play("Anim_Player_jump");
+        }
+
+// run
+        if(Input.GetKey("r"))
+        {
+            moveSpeed = defSpeed*runMultiplier;
+        } else  {
+            moveSpeed = defSpeed;
+        }
+// crouch
+   if (Input.GetKey("s")&& isGrounded){
+      animator.Play("Anim_Player_crouch");
+      //Debug.Log ("jej");
+        isCrouching = true;
+        canMove = false;
+
+    } else {
+        isCrouching = false;
+         canMove = true;
+    }
+
+    //Ledge grab
+
+
+    if ((!faceRight) &&
+        (Physics2D.Linecast(transform.position, grabCheckR.position, 1<< LayerMask.NameToLayer("Ground"))) ){// ||
+        //(Physics2D.Linecast(transform.position, grabCheckL.position, 1<< LayerMask.NameToLayer("Ground")))){
+            animator.Play("Anim_Player_grab");
+            rb2d.velocity = new Vector2(0, 0);
+            canMove = false;
+            hanging = true;
+           // Debug.Log ("grabbbbb");
+
+
+        } else {
+            hanging = false;
+        }
+     
+// up look and climb
+
+    if ((Input.GetKey ("w")) && hanging){
+        Debug.Log ("irti");
+
+    }
+
+
+    }
+
+
+}
